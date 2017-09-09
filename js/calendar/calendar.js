7ff550e51fece4f8cd8a5fc58e3c7874a0da0f7a
surya herdiyanto <suryaherdiyanto@gmail.com>
2017-09-09 22:32:56 +0700

    add next and prev

--- js/calendar/calendar.js
+++ js/calendar/calendar.js
@@ -1,14 +1,47 @@
 function Calendar(){
+
+    this.date = new Date();
+    this.currentDate = this.date.getDate();
+    this.currentMonth = this.date.getMonth();
+    this.currentYear = this.date.getFullYear();
+    this.getFirstDay = new Date(this.currentYear, this.currentMonth, 1);
+    this.monthLimit = 30;
+    
+    this.nextMonth = function(){
+        var next = this.currentMonth + 1;
+        this.currentMonth = next;
+        if(next > 11){
+            this.currentYear += 1;
+            return 1;
+        }else
+            return next;
+    }
+    this.prevMonth = function () {
+        var prev = this.currentMonth - 1;
+        this.currentMonth = prev;
+        if (prev < 2) {
+            this.currentYear -= 1;
+            return 12;
+        } else
+            return prev;
+    }
+
+    this.toNextMonth = function(){
+        this.currentMonth = this.nextMonth();
+        this.init();
+    }
+    this.toPrevMonth = function () {
+        this.currentMonth = this.prevMonth();
+        this.init();
+    }
     this.init = function(){
-        var thisDate = new Date();
-        var date = new Date(thisDate.getFullYear(), thisDate.getMonth(), 1);
         var table = $('#calendar');
-        var thisMonthDay = date.getDay();
+        var thisMonthDay = this.getFirstDay.getDay();
         var html = "<table class='table'>";
         var monthDate = 1;
-        var monthLimit = 30;
+        console.log(this.currentYear);
 
-        html += "<tr><td colspan='7' align='center'><b>" + this.toMonth(thisDate.getMonth()) + ' ' + thisDate.getFullYear()+ "</b></td></tr>";
+        html += "<tr><td colspan='7' align='center'><b>" + this.toMonth(this.currentMonth) + ' ' + this.currentYear+ "</b></td></tr>";
         html += "<tr><td><b>Minggu</b></td>";
         html += "<td><b>Senin</b></td>";
         html += "<td><b>Selasa</b></th>";
@@ -20,17 +53,21 @@ function Calendar(){
         for (var i = 0; i < 6; i++) {
             html += "<tr>";
             for (var o = 0; o < 7; o++) {
-                if (o < thisMonthDay && monthDate == 1 || monthDate > monthLimit) {
+                if (o < thisMonthDay && monthDate == 1 || monthDate > this.monthLimit) {
                     html += '<td>&nbsp;</td>';
                 } else if (monthDate >= 1) {
-                    html += '<td>' + monthDate + '</td>';
+                    if(this.currentDate == monthDate && this.date.getMonth() == this.currentMonth)
+                        html += '<td align="center" style="background: #ccc">' + monthDate + '</td>';
+                    else
+                        html += '<td align="center">' + monthDate + '</td>';
+
                     monthDate++;
                 }
             }
             html += "</tr>";
         }
 
-        table.append(html);
+        table.html(html);
     };
 
     this.toDay = function(day){
@@ -61,37 +98,37 @@ function Calendar(){
 
     this.toMonth = function(month){
         switch (month) {
-            case 0:
+            case 1:
                 return "January";
                 break;
-            case 1:
+            case 2:
                 return "February";
                 break;
-            case 2:
+            case 3:
                 return "March";
                 break;
-            case 3:
+            case 4:
                 return "April";
                 break;
-            case 4:
+            case 5:
                 return "May";
                 break;
-            case 5:
+            case 6:
                 return "June";
                 break;
-            case 6:
+            case 7:
                 return "July";
                 break;
-            case 7:
+            case 8:
                 return "August";
                 break;
-            case 8:
+            case 9:
                 return "September";
                 break;
-            case 9:
+            case 10:
                 return "October";
                 break;
-            case 10:
+            case 11:
                 return "November";
                 break;
             default:
